9aa0b03b7f4c10f90cfaf9439e9ed6188848a9f3
Andrew Ananda <andrewtiema@gmail.com>
2020-01-31 15:50:24 +0300

    feature/notesActivity

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.notes
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.notes.db.Word
 import com.example.notes.recyclerview.WordListAdapter
+import com.example.notes.viewmodels.WordViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val newWordActivityRequestCode = 1
+    private lateinit var wordViewModel: WordViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +28,38 @@ class MainActivity : AppCompatActivity() {
         val adapter = WordListAdapter(this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        wordViewModel = ViewModelProvider(this).get(WordViewModel::class.java)
+
+        //set observer to update words in the database
+        wordViewModel.allWords.observe(this, Observer { words->
+            //update cached copy
+            words?.let {
+                adapter.setWords(it)
+            }
+        })
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
     }
-}
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+
+            if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+                data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let {
+                    val word = Word(it)
+                    wordViewModel.insert(word)
+                }
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+    }
